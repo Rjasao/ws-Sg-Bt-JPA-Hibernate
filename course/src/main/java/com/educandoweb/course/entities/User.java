bf29fe89583ec20dcdf022cb7502f7c6c65f96b7
Ricardo Tosi Bela <69743502+Rjasao@users.noreply.github.com>
2022-09-28 02:45:10 -0300

    H2 database, test profile, JPA
    Banco de dados H2 - apostila 25

--- course/src/main/java/com/educandoweb/course/entities/User.java
+++ course/src/main/java/com/educandoweb/course/entities/User.java
@@ -8,12 +8,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class User implements Serializable{
-	
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)// para os imcrementos serem automaticos
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
--- course/src/main/java/com/educandoweb/course/resources/UserResource.java
+++ course/src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.educandoweb.course.entities.User;
 
 @RestController
-@RequestMapping(value = "/users") //controlador resting
+@RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "119657515", "1234567");
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}
 }
