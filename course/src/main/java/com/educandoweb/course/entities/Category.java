91c146b028759f11620c648bc7a222b63e399d4c
Ricardo Tosi Bela <69743502+Rjasao@users.noreply.github.com>
2022-10-03 04:33:11 -0300

    voltando mudancas HEROKU

--- course/src/main/java/com/educandoweb/course/entities/Category.java
+++ course/src/main/java/com/educandoweb/course/entities/Category.java
@@ -4,14 +4,14 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_category")
